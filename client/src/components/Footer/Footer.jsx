17aaeb0022259572ec2af09973abce73b9396bd8
Wolfscot <gkrasteva88@gmail.com>
2023-12-16 16:12:14 +0200

    Structureupdate, divided by pages, components

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    
+}
+export default Footer
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useState } from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+import Nav from 'react-bootstrap/Nav';
+
+function Navbar() {
+    return (
+      <Nav>
+        
+      </Nav>
+    );
+  }
+  
+  export default Navbar;
