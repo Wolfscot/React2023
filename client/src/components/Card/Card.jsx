b39619a2dee03318363bb155fa80cd808a23052b
Wolfscot <gkrasteva88@gmail.com>
2023-12-18 11:35:54 +0200

    pagination

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,8 +3,17 @@ import { FiCalendar, FiClock, FiDollarSign, FiMapPin, FiSearch } from "react-ico
 import { Link } from "react-router-dom";
 
 const Card = ({ data }) => {
-   console.log(data);
-  const {_id,companyLogo, jobTitle, companyName, jobLocation, employmentType, minPrice,maxPrice, postingDate, description} = data;
+  console.log(data);
+  const { _id,
+    companyLogo,
+    jobTitle,
+    companyName,
+    jobLocation,
+    employmentType,
+    minPrice,
+    maxPrice,
+    postingDate,
+    description } = data;
   return (
     <div>
       <section className="card">
@@ -15,10 +24,10 @@ const Card = ({ data }) => {
             <h3 className="text-lg font-semibold mb-2">{jobTitle}</h3>
 
             <div className="text-primary/70 text-base flex flex-wrap gap-2 mb-2">
-              <span className="flex items-center gap-2"><FiMapPin/> {jobLocation}</span>
-              <span className="flex items-center gap-2"><FiClock/> {employmentType}</span>
-              <span className="flex items-center gap-2"><FiDollarSign/> {minPrice}-{maxPrice}k</span>
-              <span className="flex items-center gap-2"><FiCalendar/> {postingDate}</span>
+              <span className="flex items-center gap-2"><FiMapPin /> {jobLocation}</span>
+              <span className="flex items-center gap-2"><FiClock /> {employmentType}</span>
+              <span className="flex items-center gap-2"><FiDollarSign /> {minPrice}-{maxPrice}k</span>
+              <span className="flex items-center gap-2"><FiCalendar /> {postingDate}</span>
             </div>
 
             <p className="text-base text-primary/70 ">{description}</p>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,6 +10,9 @@ const Home = () => {
   const [selectedCategory, setselectedCategory] = useState(null);
   const [jobs, setJobs] = useState([]);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+
   useEffect(() => {
     fetch("jobs.json").then(res => res.json())
       .then(data => {
@@ -22,11 +25,11 @@ const Home = () => {
   // handlers for search and filtering
   const handleInputChange = (event) => {
     setQuery(event.target.value);
-  };  
-  
+  };
+
   const handleChange = (event) => {
     setSelectedCategory(event.target.value);
-    
+
   };
   const handleClick = (event) => {
     setSelectedCategory(event.target.value);
@@ -41,7 +44,7 @@ const Home = () => {
     let filteredJobs = jobs;
     // Filtering Input Items
 
-    
+
     //Applying input search field
     if (query) {
       filteredJobs = filteredItems;
@@ -70,17 +73,74 @@ const Home = () => {
     }
 
     return filteredJobs.map((data, i) => <Card key={i} data={data} />);
-   
+
   };
 
   const result = filteredData(jobs, selectedCategory, query);
 
+  // pagination/
+  const calculatePageRange = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return { startIndex, endIndex };
+  };
 
+  //  next page
+  const nextPage = () => {
+    if (currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  //  prev page
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
-      <div>
-          <Jobs result={result}/>
+      <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+        <div className="bg-white p-4 rounded">
+         
+        </div>
+        <div className="col-span-2 bg-white p-4 rounded">
+          
+            
+        
+            <Jobs result={result} />
+         
+
+          {/* pagination  */}
+
+          {result.length > 0 ? (
+            <div className="flex justify-center mt-4 space-x-8">
+              <button
+                onClick={prevPage}
+                disabled={currentPage === 1}
+                className="hover:underline"
+              >
+                Previous
+              </button>
+              <span className="mx-2">
+                Page {currentPage} of{" "}
+                {Math.ceil(filteredItems.length / itemsPerPage)}
+              </span>
+              <button
+                onClick={nextPage}
+                disabled={
+                  currentPage === Math.ceil(filteredItems.length / itemsPerPage)
+                }
+                className="hover:underline"
+              >
+                Next
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>        
       </div>
     </div>
   )
